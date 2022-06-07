d899fb5022fdb49c5f2430f8f5512295b6b5c0ac
rrababah <rrababah@atypon.comn>
2022-06-07 14:23:54 +0300

    VEERSION-1

--- src/services/searchApiService.js
+++ src/services/searchApiService.js
@@ -1,9 +1,10 @@
 import {useDispatch} from "react-redux";
-import {useEffect} from "react";
+import {useEffect, useSelector} from "react";
 import {recipeInformationActions} from "../store/recipe-state/recipeInformation";
+import axios from 'axios';
 
-const GetRecipeInformation = () =>{
-    const dispatch =useDispatch(),
+const GetRecipeInformation = () => {
+    const dispatch = useDispatch(),
         pathName = window.location.pathname,
         recId = pathName.split('id=')[1];
 
@@ -18,5 +19,15 @@ const GetRecipeInformation = () =>{
         });
 
     }, []);
+
 }
-export {GetRecipeInformation}
+
+const GetSearchResults = async () => {
+    const
+        enteredRecipe = useSelector(state => state.enteredRecipeSlice.enteredRecipe);
+    const response = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=7b2e03b0ee4b496eb98cd157dd51f32c&query=${enteredRecipe}&number=30`);
+    return await response.json();
+
+    // return(<div></div>);
+}
+export {GetRecipeInformation, GetSearchResults}
